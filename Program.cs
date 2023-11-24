d2297a10a40d4ef4c6cedb26df60fd2cf85fe80d
mohammadKazem <mmadkzm@gmail.com>
2023-11-24 21:14:53 +0330

    Moified and added Controller for api

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using School.Data.Context;
+using School.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(option =>
 {
     option.UseNpgsql(builder.Configuration.GetConnectionString("SchoolConnectionStrings"));
 });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IStudentRepository, StudentRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +26,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 app.Run();
 
