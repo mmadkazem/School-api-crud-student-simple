c018740e7a5bb1b3790be087b9445e2d81cd98c9
mohammadKazem <mmadkzm@gmail.com>
2023-11-24 17:12:35 +0330

    first Migrations

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using School.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseNpgsql(builder.Configuration.GetConnectionString("SchoolConnectionStrings"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
